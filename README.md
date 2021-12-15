ad4aaadf3403d000fdb47537a069031c212d229e
Alenygam <alessandro@alenygam.com>
2021-12-15 17:13:31 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# spaggiu-server
+
+Semplice proxy per l'api di classeviva, in modo da poterla utilizzare su una web app.
+
+[API endpoints](https://github.com/michelangelomo/Classeviva-Official-Endpoints)
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import fetch from 'node-fetch';
+
+const app: express.Application = express();
+
+const port: number = Number(process.env.PORT) || 5000;
+
+app.use((_req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+app.use(express.json());
+
+const apiBaseURL : string = "https://web.spaggiari.eu/rest/v1";
+const necessaryHeaders = {
+    'Content-Type': 'application/json',
+    'Z-Dev-ApiKey': '+zorro+',
+    'User-Agent': 'zorro/1.0'
+}
+
+app.post('/auth/login', async (_req, _res) => {
+    try {
+        const res = await fetch(`${apiBaseURL}/auth/login`, {
+            method: "POST",
+            body: JSON.stringify(_req.body),
+            headers: necessaryHeaders
+        });
+
+        _res.status(res.status).json(await res.json());
+    } catch (err: any) {
+        _res.status(500).json({"message": err.message});
+    }
+})
+
+app.listen(port, () => {
+    console.log("Typescript with express");
+})
