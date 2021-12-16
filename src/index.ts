094227e4c815fc59893b8175b688f97a0718ee37
Alenygam <alessandro@alenygam.com>
2021-12-16 17:02:46 +0100

    Fixed my stupidity again

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const port: number = Number(process.env.PORT) || 5000;
 
 app.use((_req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Z-Auth-Token");
     next();
 })
 app.use(express.json());
